9133d4753a38afbca0625cde105654baa6a68176
EC2 Default User <ec2-user@ip-172-31-80-213.ec2.internal>
2019-08-29 02:53:57 +0000

    My first commit

---
+++ labwork/lab1.php
@@ -0,0 +1,36 @@
+<!DOCtype html>
+<html>
+    <head>
+        <title>Labwork 1: Hello World</title>
+        <style>
+            body {
+                background-color: #D6EAF8;
+                color: #808B96;
+                font-size: 1.5em;
+                line-height: 1.3em;
+                padding: 2%;
+            }
+            h1 {
+                font-size: 1.75em;
+            }
+            p {
+                text-align: center;
+                width: 65%;
+            }
+            
+        </style>
+    </head>
+<body>
+    <h1>Hello World!</h1>
+    <p>My name is Holly Nahigyan and I am a Web Development student at Long Beach City College.
+    Prior to starting the Web Dev program at LBCC I had essentially no experience with coding. 
+    However, I've found coding to be a challenging and rewarding endeavor.</p>
+    <p>Here's a link to my <a href="https://github.com/hollyLikestoCode" target="_blank">GitHub Prolie</a></p>
+    <p>Outside of my academic interests I enjoy hiking, cooking and working out. I mainly hike around the Angeles National Forest and go about once every week or two.
+    I love to cook all types of recipes and cusines. Lately, I've been trying to eat better and have been making primarily vegetarian at home.</p>
+    
+</body>
+</html>
+<?php
+    echo 'This is a string.';
+?>
