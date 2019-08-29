65a685a95f1cdbb3e7f6e2593d966f0839ab35b6
EC2 Default User <ec2-user@ip-172-31-80-213.ec2.internal>
2019-08-29 22:06:17 +0000

    Final edit

--- labwork/lab1.php
+++ labwork/lab1.php
@@ -39,11 +39,10 @@
     <p>Outside of my academic interests I enjoy hiking, cooking and working out. I mainly hike around the Angeles National Forest and go about once every week or two.
     I love to cook all types of recipes and cusines. Lately, I've been trying to eat  and have been making primarily vegetarian at home.</p>
     <?php
-        echo 'This my PHP pretty paragraph.';
         echo '<p>Some of the websites I use the most are <a href="https://www.alltrails.com/" target="_blank">AllTrails</a> and <a href="https://www.instagram.com/" target="_blank"> Instagram</a>.</p>
         <p>AllTrails is helpful site for finding hiking trails and other outdoor activities in your area. Instagram is my preferred social media site. Great for sharing pictures, humor, and connecting with friends.</p>';
-        echo "<p>This assignment was relatively easy. I found the instructions easy to follow and working in class helped me figure out the things that weren't obvious</p>
-        <p>The only problem I had was even after linking to GitHub and Heroku and uploading the php file I still can't get the page to display.</p>"; 
+        echo "<p>This assignment was a little challenging but easy to follow. I found the instructions to be very straightforward and working in class helped for when I did get stuck on something.</p>
+        <p>I had a little trouble just figuring out how to view the page on Heroku, but connecting all the sites and uploading the file was easy.</p>"; 
         
     ?>
 </body>
